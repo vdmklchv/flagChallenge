3b882f4addd047463b45d28f5ea91f39252b40ea
Vadim Colcev <vadimkolchev@gmail.com>
2020-11-19 19:18:44 +0200

    Initial Commit

---
+++ FlagChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlagChallenge
+//
+//  Created by Vadim Colcev on 11/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
